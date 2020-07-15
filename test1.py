dfc4231324f870fff7b94c4cd76cf7668ac75e58
jerryzhao173985 <44931279+jerryzhao173985@users.noreply.github.com>
2020-07-15 14:00:42 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -12,14 +12,15 @@ try:
    while True:
        data=ultrasonic.get_distance()   #Get the value
        print ("Obstacle distance is "+str(data)+"CM")
-       if(data<10):
+       
+      if(data<10):
            PWM.setMotorModel(-1000,-1000,-1000,-1000)   #Back
            print ("The car is going backwards")  
            time.sleep(1)
-        else:
+      else:
            PWM.setMotorModel(1000,1000,1000,1000)       #Forward
            print ("The car is moving forward")
            time.sleep(1)
                 
 except KeyboardInterrupt:
-    print ("\nEnd of program")
+  print ("\nEnd of program")
